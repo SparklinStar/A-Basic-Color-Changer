385b9e4ce50c7c3018eefdaa4a6352ddbde28aed
sparklinstar <souradip1000@gmail.com>
2022-12-25 00:30:35 +0530

    Final

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-const btn = document.querySelector(".Button");
+const btn = document.querySelector(".button");
+const color = document.querySelector(".color");
+
 btn.addEventListener("click", function() 
 {
     let hexColor = "#";
@@ -7,8 +9,10 @@ btn.addEventListener("click", function()
     {
         hexColor += hex[getRandomNumber()];
     }
-
+   
     document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+    color.style.color = hexColor;
 }
 
 );
